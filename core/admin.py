9b5650d447a8dd56770d94e8987008ec8394d8a4
kiselevdi <d.kiselev18@yandex.ru>
2019-05-18 16:52:36 +0300

    Init project commit.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(MilitaryUnits)
+admin.site.register(Events)
+admin.site.register(ResponsiblePersons)
--- core/models.py
+++ core/models.py
@@ -1,12 +1,42 @@
 from django.db import models
 
 
-class MilitaryUnit(models.Model):
+class MilitaryUnits(models.Model):
     Name = models.CharField(max_length=200, verbose_name="Название воинской части")
     Number = models.IntegerField(verbose_name="Номер воинской части", null=True, blank=True)
 
     def __str__(self):
         return self.Name
 
+    class Meta:
+        verbose_name = "Воинские части"
+        verbose_name_plural = "1. Воинские части"
 
-class
+
+class ResponsiblePersons(models.Model):
+    MilitaryUnitsID = models.ForeignKey(MilitaryUnits, verbose_name="Название воинской части", on_delete=models.CASCADE)
+    Name = models.CharField(max_length=200, verbose_name="Наименование должности ответственного лица")
+    ShortName = models.CharField(max_length=200, verbose_name="Краткое наименование должности ответственного лица")
+
+    def __str__(self):
+        return self.ShortName
+
+    class Meta:
+        verbose_name = "Ответственные исполнители"
+        verbose_name_plural = "2. Ответственные исполнители"
+
+
+class Events(models.Model):
+    ResponsiblePersonsID = models.ForeignKey(ResponsiblePersons, verbose_name="Ответственный", on_delete=models.CASCADE)
+    Name = models.CharField(max_length=300, verbose_name="Название мероприятия")
+    StartDate = models.DateField(verbose_name="Дата начала мероприятия")
+    EndDate = models.DateField(verbose_name="Дата окончания мероприятия")
+    StartTime = models.TimeField(verbose_name="Время начала мероприятия")
+    EndTime = models.TimeField(verbose_name="Время окончания мероприятия")
+
+    def __str__(self):
+        return self.Name
+
+    class Meta:
+        verbose_name = "Основные мероприятия"
+        verbose_name_plural = "3. Основные мероприятия"
